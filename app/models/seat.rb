84ca604c6df7ae288162e67d176055c2517750a6
Bisaso Ronald <bronnie313@gmail.com>
2024-02-10 08:26:14 +0300

    add custom validation to check flight capacity

--- app/models/seat.rb
+++ app/models/seat.rb
@@ -1,2 +1,16 @@
 class Seat < ApplicationRecord
+    belongs_to :flight
+
+    validate :validate_baggage_allowance
+
+    private
+
+    def validate_baggage_allowance
+        if baggage > flight.baggage_allowance
+            errors.add(:base, "You have too much baggage")
+        end
+        if flight.seats.size >= flight.capacity
+            errors.add(:base, "The flight is fully booked")
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'flights#index'
   resources :seats
   resources :flights
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
